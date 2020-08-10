6feaeb551ac3be2f35af0cc3ddf30d1a7e287bcf
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-10 13:47:59 -0300

    feat: Adding Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+export default () => {
+    return (
+        <header id="header" className="App-header">
+            <nav className="App-nav">
+                <h2 id="logo">
+                    <a href="/">GitHub <span>Jobs</span> </a>
+                </h2>
+                <ul>
+                    <li>
+                        <a href="/">All jobs</a>
+                    </li>
+                    <li>
+                        <a href="/">Post a job</a>
+                    </li>
+                    <li>
+                        <a href="/">How it Works</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
