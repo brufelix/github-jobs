08f8517d1ba302050d684f2925f6a2fbc9c9b973
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-10 15:51:24 -0300

    feat: adding search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
+import Search from './components/Search'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Search/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/Search.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import {FaSearch } from 'react-icons/fa'
+import { GoLocation } from 'react-icons/go'
+import './Search.css'
+
+export default () => {
+    return(
+        <div className="App-search">
+            <div className="search-field-containes">
+                <div className="box">
+                    <FaSearch style={{marginRight: "15px", marginLeft: "5px"}}/>
+                    <input name="Job Description" type="text" placeholder="Filter by title, benefits, companies, expertise"/> 
+                    <label htmlFor="Job Description" className="label-name">Job Description</label>
+                </div>
+                |  
+                <div className="box">
+                    <GoLocation style={{marginRight: "15px", marginLeft: "12px"}}/>
+                    <input name="Location" type="text" placeholder="Filtrer by city, state, zip code or country"/>
+                    <label className="label-name" htmlFor="Location">Location</label>
+                </div>
+                <button >Search</button>
+            </div>
+        </div>
+    )
+}
