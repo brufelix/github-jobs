60d79e16ba4ba58a94e81f8073febd0165ecf1f3
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-11 15:14:25 -0300

    refactor: Changing cores and adding paging button

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -4,21 +4,23 @@ import JobCard from './JobCard'
 
 export default () => {
     return(
-        <div className="App-JobOpportunity-container">
-            <JobCard fullTime={true}  location="Rio De Janeiro" nameComporation="Insigth lab" 
-                requirements="JS/TS, MongoDB, Knex, ReactJS, Git" time={3} 
-                title="Desenvolvedor Frontend Junior"/>
-            <JobCard fullTime={false}  location="Rio De Janeiro" nameComporation="Insigth lab" 
-                requirements="JS/TS, MongoDB, Knex" time={3} 
-                title="Java and Python Engineering Opportunities!"/>
-            <JobCard fullTime={false}  location="Rio De Janeiro" nameComporation="Insigth lab" 
-                requirements="JS/TS, MongoDB, Knex, ReactJS, Git, JS/TS, MongoDB, Knex, ReactJS, Git
-                JS/TS, MongoDB, Knex, ReactJS, GitJS/TS, MongoDB, Knex, 
-                ReactJS, GitJS/TS, MongoDB, Knex, ReactJS, Git, JS/TS, MongoDB, Knex, ReactJS, GitJS/TS, MongoDB, Knex, ReactJS, Git" 
-                time={3} 
-                title="Technical Infrastructure Implementation Healthcare Project Manager"/>
-            <JobCard fullTime={true}  location="Rio De Janeiro" nameComporation="Insigth lab" 
-                requirements="JS/TS, MongoDB, Knex, ReactJS, Git" time={3} title="Dev Junior"/>
-        </div>
+        <React.StrictMode>
+            <div className="App-JobOpportunity-container">
+                <JobCard fullTime={true}  location="Rio De Janeiro" nameComporation="Insigth lab" 
+                    requirements="JS/TS, MongoDB, Knex, ReactJS, Git" time={3} 
+                    title="Desenvolvedor Frontend Junior"/>
+                <JobCard fullTime={false}  location="Rio De Janeiro" nameComporation="Insigth lab" 
+                    requirements="JS/TS, MongoDB, Knex" time={3} 
+                    title="Java and Python Engineering Opportunities!"/>
+                <JobCard fullTime={false}  location="Rio De Janeiro" nameComporation="Insigth lab" 
+                    requirements="JS/TS, MongoDB, Knex, ReactJS, Git, JS/TS, MongoDB, Knex, ReactJS, Git" 
+                    time={3}  title="Technical Infrastructure Implementation Healthcare Project Manager"/>
+                <JobCard fullTime={true}  location="Rio De Janeiro" nameComporation="Insigth lab" 
+                    requirements="JS/TS, MongoDB, Knex, ReactJS, Git" time={3} title="Dev Junior"/>
+            </div>
+            <div className="div-pagination">
+                <button id="button-pagination">More Awesome Jobs</button>
+            </div>
+        </React.StrictMode>
     )
 }
