66609fee1c2f244fce89009b4977e30fbb567044
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-11 18:52:40 -0300

    feat: Adding base url for requests

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,25 +1,24 @@
 import React from 'react'
-import './Feed.css'
 import JobCard from './JobCard'
+import { BASEURL, headers } from '../config/config'
+import './Feed.css'
 
 export default () => {
+    const getJobs = async () => {
+        fetch(`${BASEURL}positions.json?description=python&location=new+york`, {headers, mode: "cors"})
+            .then(res => res.json())
+            .then(res => console.log(res))
+            .catch(() =>  console.log("Error"))
+    }
     return(
         <React.StrictMode>
             <div className="App-JobOpportunity-container">
                 <JobCard fullTime={true}  location="Rio De Janeiro" nameComporation="Insigth lab" 
                     requirements="JS/TS, MongoDB, Knex, ReactJS, Git" time={3} 
                     title="Desenvolvedor Frontend Junior"/>
-                <JobCard fullTime={false}  location="Rio De Janeiro" nameComporation="Insigth lab" 
-                    requirements="JS/TS, MongoDB, Knex" time={3} 
-                    title="Java and Python Engineering Opportunities!"/>
-                <JobCard fullTime={false}  location="Rio De Janeiro" nameComporation="Insigth lab" 
-                    requirements="JS/TS, MongoDB, Knex, ReactJS, Git, JS/TS, MongoDB, Knex, ReactJS, Git" 
-                    time={3}  title="Technical Infrastructure Implementation Healthcare Project Manager"/>
-                <JobCard fullTime={true}  location="Rio De Janeiro" nameComporation="Insigth lab" 
-                    requirements="JS/TS, MongoDB, Knex, ReactJS, Git" time={3} title="Dev Junior"/>
             </div>
             <div className="div-pagination">
-                <button id="button-pagination">More Awesome Jobs</button>
+                <button id="button-pagination" onClick={() => getJobs()}>More Awesome Jobs</button>
             </div>
         </React.StrictMode>
     )
---
+++ src/config/config.ts
@@ -0,0 +1,12 @@
+const URL: string =  "https://jobs.github.com/"
+const proxyurl: string = "https://cors-anywhere.herokuapp.com/"
+
+export const BASEURL: string = `${URL}`
+
+export const headers = new Headers();
+
+headers.append('Content-Type', 'application/json');
+headers.append('Accept', 'application/json')
+headers.append('Access-Control-Allow-Origin', '*')
+headers.append('Access-Control-Allow-Credentials', 'true')
+headers.append('GET', 'POST')
