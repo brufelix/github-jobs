c850669408e2a60f4259704f1dcf8c5a05a19cdc
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-10 23:34:46 -0300

    feat: adding component Feed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/Header'
 import Search from './components/Search'
+import Feed from './components/Feed'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header />
       <Search/>
+      <Feed/>
     </div>
   )
 }
---
+++ src/components/Feed.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './Feed.css'
+
+export default () => {
+    return(
+        <div className="App-JobOpportunity-container">
+            <div className="Box-JobOpportunity">
+                <div className="title">Senior PHP Developer</div>
+                <div className="Subtitle-Times">
+                    <div className="time">Há cerca de 16h</div>
+                    <div className="full-time">Full Time</div>
+                </div>
+                <div className="location">Los Angeles</div>
+                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
+                <div id="nameComporation">name of comporation</div>
+                <button id="apply-button">Apply Now</button>
+            </div>
+            <div className="Box-JobOpportunity">
+                <div className="title">Senior PHP Developer</div>
+                <div className="Subtitle-Times">
+                    <div className="time">Há cerca de 16h</div>
+                    <div className="full-time">Full Time</div>
+                </div>
+                <div className="location">Los Angeles</div>
+                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
+                <div id="nameComporation">name of comporation</div>
+                <button id="apply-button">Apply Now</button>
+            </div>
+            <div className="Box-JobOpportunity">
+                <div className="title">Senior PHP Developer</div>
+                <div className="Subtitle-Times">
+                    <div className="time">Há cerca de 16h</div>
+                    <div className="full-time">Full Time</div>
+                </div>
+                <div className="location">Los Angeles</div>
+                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
+                <div id="nameComporation">name of comporation</div>
+                <button id="apply-button">Apply Now</button>
+            </div>
+            <div className="Box-JobOpportunity">
+                <div className="title">Senior PHP Developer</div>
+                <div className="Subtitle-Times">
+                    <div className="time">Há cerca de 16h</div>
+                    <div className="full-time">Full Time</div>
+                </div>
+                <div className="location">Los Angeles</div>
+                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
+                <div id="nameComporation">name of comporation</div>
+                <button id="apply-button">Apply Now</button>
+            </div>
+        </div>
+        
+    )
+}
