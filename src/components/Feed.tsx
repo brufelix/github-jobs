ed69188f57032d17e2d82fd6986fe1ab26a8eaf6
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-11 10:14:37 -0300

    refactor: Creating JobCard component

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,53 +1,16 @@
 import React from 'react'
 import './Feed.css'
+import JobCard from './JobCard'
 
 export default () => {
     return(
         <div className="App-JobOpportunity-container">
-            <div className="Box-JobOpportunity">
-                <div className="title">Senior PHP Developer</div>
-                <div className="Subtitle-Times">
-                    <div className="time">Há cerca de 16h</div>
-                    <div className="full-time">Full Time</div>
-                </div>
-                <div className="location">Los Angeles</div>
-                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
-                <div id="nameComporation">name of comporation</div>
-                <button id="apply-button">Apply Now</button>
-            </div>
-            <div className="Box-JobOpportunity">
-                <div className="title">Senior PHP Developer</div>
-                <div className="Subtitle-Times">
-                    <div className="time">Há cerca de 16h</div>
-                    <div className="full-time">Full Time</div>
-                </div>
-                <div className="location">Los Angeles</div>
-                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
-                <div id="nameComporation">name of comporation</div>
-                <button id="apply-button">Apply Now</button>
-            </div>
-            <div className="Box-JobOpportunity">
-                <div className="title">Senior PHP Developer</div>
-                <div className="Subtitle-Times">
-                    <div className="time">Há cerca de 16h</div>
-                    <div className="full-time">Full Time</div>
-                </div>
-                <div className="location">Los Angeles</div>
-                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
-                <div id="nameComporation">name of comporation</div>
-                <button id="apply-button">Apply Now</button>
-            </div>
-            <div className="Box-JobOpportunity">
-                <div className="title">Senior PHP Developer</div>
-                <div className="Subtitle-Times">
-                    <div className="time">Há cerca de 16h</div>
-                    <div className="full-time">Full Time</div>
-                </div>
-                <div className="location">Los Angeles</div>
-                <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
-                <div id="nameComporation">name of comporation</div>
-                <button id="apply-button">Apply Now</button>
-            </div>
+            <JobCard/>
+            <JobCard/>
+            <JobCard/>
+            <JobCard/>
+            <JobCard/>
+            <JobCard/>
         </div>
         
     )
---
+++ src/components/JobCard.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './JobCard.css'
+
+export default () => {
+    return (
+        <div className="Box-JobOpportunity">
+            <div className="title">Senior PHP Developer</div>
+            <div className="Subtitle-Times">
+                <div className="time">Há cerca de 16h</div>
+                <div className="full-time">Full Time</div>
+            </div>
+            <div className="location">Los Angeles</div>
+            <div id="requirements">Ts/Js, Css, html, unix, bash, typescript, typescript.</div>
+            <div id="nameComporation">name of comporation</div>
+            <button id="apply-button">Apply Now</button>
+        </div>
+    )
+}
