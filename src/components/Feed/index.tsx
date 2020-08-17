61d76768273f3d03ab314383e9c570fafd568ed4
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-17 11:21:51 -0300

    feat: Adding hot searches in Feed component

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,10 +1,10 @@
 import React, {Component} from 'react'
 import { connect, ConnectedProps } from 'react-redux'
-import Search from '../Search/'
 
 import mapStateToProps from './mapStateToProps'
 import mapDispatchToProps from './mapDispatchToProps'
 import { TJobCard, TPropsFeed } from '../../types/types'
+import Search from '../Search/'
 import JobCard from '../JobCard/'
 import './Feed.css'
 
@@ -15,6 +15,7 @@ class Feed extends Component<Props> {
     constructor(props: Props) {
         super(props)
         this.getMoreJobs.bind(this)
+        this.handleClickSearch.bind(this)
     }
 
     getMoreJobs() {
@@ -36,6 +37,19 @@ class Feed extends Component<Props> {
         }
     }
 
+    handleClickSearch(description: string, location: string = "") {
+        const { searchCache, updateIsSearch, resettingStartEndValues, clearJobsCache,
+            clearJobsVisible, initializePages, clearValuleExpectedCache } = this.props
+            
+            initializePages()
+            clearJobsVisible()
+            clearJobsCache()
+            resettingStartEndValues()
+            clearValuleExpectedCache()
+            searchCache(1, description, location)
+            updateIsSearch(true)
+        }
+
     componentDidUpdate() {
         const { isSearch, end, jobsCache, updateEndAndStart, 
             updateIsSearch, updateEndJobs, updateJobsVisible } = this.props
@@ -56,7 +70,7 @@ class Feed extends Component<Props> {
     }
 
     render(){
-        const { jobsVisible, endJobs} = this.props
+        const { jobsVisible, endJobs } = this.props
         
         return (
             <React.StrictMode>
@@ -67,6 +81,20 @@ class Feed extends Component<Props> {
                         <JobCard company={job.company} created_at={job.created_at} location={job.location} 
                         title={job.title} type={job.type} key={index} id={job.id} />)}
                 </div>
+                { endJobs && 
+                    (<div className="hotSerches">
+                    <h2>Hot Searches</h2>
+                    <div className="buttons">
+                        <button onClick={() => this.handleClickSearch("javascript")}>JavaScript</button>
+                        <button onClick={() => this.handleClickSearch("python")}>Python</button>
+                        <button onClick={() => this.handleClickSearch("Linux")}>Linux</button>
+                        <button onClick={() => this.handleClickSearch("","Europe")} >Europe</button>
+                        <button onClick={() => this.handleClickSearch("","London")}>London</button>
+                        <button onClick={() => this.handleClickSearch("PHP")}>PHP</button>
+                        <button onClick={() => this.handleClickSearch("Android")}>Android</button>
+                        <button onClick={() => this.handleClickSearch("Rails")}>Rails</button>
+                    </div>
+                </div>)}
                 <div className="div-pagination">
                     {!endJobs &&
                         <button id="button-pagination" onClick={() => this.getMoreJobs()}>More Awesome Jobs</button>}
