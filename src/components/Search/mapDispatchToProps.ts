34165407c4a66dda06dc92dd229f8860b07f87c6
Bruno Felix Dias <brunofelix.ml@gmail.com>
2020-08-18 09:30:25 -0300

    refactor: Removing mapStateToProps and MapDispatchToProps files

--- src/components/Search/mapDispatchToProps.ts
+++
@@ -1,9 +0,0 @@
-import { bindActionCreators } from 'redux'
-import {search, descriptionChange, locationChanged, clearJobsCache, clearJobsVisible, 
-    clearValuleExpectedCache, clearState, resettingStartEndValues, searchCache, updatePage, 
-    initializePages, updateEndJobs, updateIsSearch, updateJobsVisible } from '../../redux/actions'
-
-export default (dispatch: any) => 
-bindActionCreators({ descriptionChange, locationChanged, search, clearJobsCache, clearJobsVisible, initializePages,clearState,
-    resettingStartEndValues, searchCache, updatePage, clearValuleExpectedCache, updateEndJobs,
-        updateIsSearch, updateJobsVisible}, dispatch)
