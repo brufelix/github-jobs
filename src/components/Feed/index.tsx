61b9fab989d3a7806bd2804d58f0b2e06f9a1217
Bruno Felix <bruno.felix@insightlab.ufc.br>
2021-02-05 13:53:02 -0300

    fix: removing from the card container, and adding padding

--- src/components/Feed/index.tsx
+++ src/components/Feed/index.tsx
@@ -1,16 +1,18 @@
-import React, {useEffect, useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { TJobCard, TStateGithubJob } from '../../types/types'
 import { searchCache, fetchInitJobs } from '../../utils/functions'
-import { updateEndAndStart, updateJobsVisible, updateEndJobs, resettingStartEndValues, clearJobsCache, 
-    clearJobsVisible, initializePages, clearValuleExpectedCache,updadeValueExpectedCache, updateIsSearch } from '../../redux/actions'
+import {
+    updateEndAndStart, updateJobsVisible, updateEndJobs, resettingStartEndValues, clearJobsCache,
+    clearJobsVisible, initializePages, clearValuleExpectedCache, updadeValueExpectedCache, updateIsSearch
+} from '../../redux/actions'
 import spinnerImg from '../../asset/spinner.svg'
 import Search from '../Search/'
 import JobCard from '../JobCard/'
 import './Feed.css'
 
-function Feed(){
+function Feed() {
 
     const dispatch = useDispatch()
     const [loading, setLoading] = useState(true)
@@ -23,9 +25,9 @@ function Feed(){
     const jobDescription = useSelector((state: TStateGithubJob) => state.githubjobs.jobDescription)
     const location = useSelector((state: TStateGithubJob) => state.githubjobs.location)
     const end = useSelector((state: TStateGithubJob) => state.githubjobs.end)
-    const listSearchButtons = ["Javascript", "Python", "Linux", "Scala","Android", "IOS", "Erlang", "Rails" ]
+    const listSearchButtons = ["Javascript", "Python", "Linux", "Scala", "Android", "IOS", "Erlang", "Rails"]
 
-    function timeOutSpinner(boolean: boolean = true ){
+    function timeOutSpinner(boolean: boolean = true) {
         if (boolean) {
             setLoading(boolean)
         } else {
@@ -36,16 +38,16 @@ function Feed(){
     }
 
     function getMoreJobs() {
-        if ( jobsCache.length === valueExpectedCache ) {
+        if (jobsCache.length === valueExpectedCache) {
             searchCache(dispatch, page, jobDescription, location)
             dispatch(updadeValueExpectedCache())
         }
-            
+
         if (jobsVisible.length < jobsCache.length) {
             dispatch(updateJobsVisible(jobsCache))
             dispatch(updateEndAndStart())
         }
-
+        // eslint-disable-next-line
         if (end > jobsCache.length) {
             dispatch(updateEndJobs(true))
         }
@@ -55,6 +57,7 @@ function Feed(){
         if (end > jobsCache.length) {
             dispatch(updateEndJobs(true))
         }
+        // eslint-disable-next-line
     }, [jobsVisible, jobsCache])
 
     useEffect(() => {
@@ -62,10 +65,11 @@ function Feed(){
         fetchInitJobs(dispatch, 1)
         dispatch(updateEndJobs(true))
         timeOutSpinner(false)
+        // eslint-disable-next-line
     }, [])
 
     function handleClickSearch(jobDescription: string) {
-        timeOutSpinner(true)   
+        timeOutSpinner(true)
         dispatch(initializePages())
         dispatch(clearJobsVisible())
         dispatch(clearJobsCache())
@@ -77,30 +81,30 @@ function Feed(){
     }
 
     return (
-        <>
-            <Search setLoad={timeOutSpinner}/> 
+        <div className="container-feed">
+            <Search setLoad={timeOutSpinner} />
             <h2 className="title-feed">Newly Added Jobs</h2>
             <div className="app-JobOpportunity-container">
                 {loading ? <img src={spinnerImg} alt="Loading" style={{ width: 250, height: 50 }}></img> :
-                    jobsVisible.map((job: TJobCard, index: number) =>  
-                        <JobCard company={job.company} created_at={job.created_at} location={job.location} 
-                           title={job.title} type={job.type} key={index} id={job.id} />)
+                    jobsVisible.map((job: TJobCard, index: number) =>
+                        <JobCard company={job.company} created_at={job.created_at} location={job.location}
+                            title={job.title} type={job.type} key={index} id={job.id} />)
                 }
             </div>
             {endJobs && !loading &&
                 (<div className="hotSerches">
                     <h2>Hot Searches</h2>
                     <div className="buttons">
-                        {listSearchButtons.map((searchName: string, index: number) => 
+                        {listSearchButtons.map((searchName: string, index: number) =>
                             <button key={index}
                                 onClick={() => handleClickSearch(searchName)}>{searchName}</button>)}
                     </div>
                 </div>)}
             <div className="div-pagination">
                 {!endJobs && !loading &&
-                   (<button id="button-pagination" onClick={() => getMoreJobs()}>More Awesome Jobs</button>)}
+                    (<button id="button-pagination" onClick={() => getMoreJobs()}>More Awesome Jobs</button>)}
             </div>
-        </>
+        </div>
     )
 }
 
